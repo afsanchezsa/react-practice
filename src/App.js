0501c089370fa1af00d3d3c029f72cc3b7776c3e
unknown <afsanchezsa@unal.edu.co>
2020-07-08 20:07:31 -0500

    before single page

--- src/App.js
+++ src/App.js
@@ -5,15 +5,18 @@ import './App.css';
 import MainComponent from './components/MainComponent';
 import Menu from './components/MenuComponent';
 import {DISHES} from './shared/dishes';
+import {BrowserRouter} from 'react-router-dom';
 class App extends React.Component{
 
 render() {
   return (
+    <BrowserRouter>
     <div>
      
       <MainComponent/>  
       
     </div>
+    </BrowserRouter>
   );
 }
 }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,34 +2,40 @@ import React from 'react';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Menu from './MenuComponent';
-import {DISHES} from '../shared/dishes';
+import { DISHES } from '../shared/dishes';
 import Dishdetail from './DishdetailComponent';
-class Main extends React.Component{
-constructor(props){
-  super(props);
-  this.state={
-    dishes:DISHES,
-    selectedDish:null
-  };
-}  
-onDishSelected(dishId){
-    this.setState({
-        selectedDish:dishId
-    });
 
-}
-render() {
-  return (
-    <div>
-      <Header/>
-     
-      <Menu dishes={this.state.dishes} onClick={(dishId)=>this.onDishSelected(dishId)}/>  
-      
-      <Dishdetail dish={this.state.dishes.filter((dish)=>dish.id===this.state.selectedDish)[0]}/>
-      <Footer/>
-    </div>
-  );
-}
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Home from './HomeComponent';
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+
+    };
+  }
+
+  render() {
+    const HomePage=()=>{
+      return(<Home/>);
+    }
+    return (
+      <div>
+        <Header />
+
+        <Switch>
+
+          <Route path="/home" component={HomePage} />
+          <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+          <Redirect to="home"/>
+        </Switch>
+
+
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default Main;
